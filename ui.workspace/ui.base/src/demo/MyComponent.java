194e509837e3bad821d367bcf5996b3ddf03b741
jeromebridge <jeromebridge@pennassurancesoftware.com>
2015-05-19 16:10:04 -0400

    First working version using Amdatu and BndTools.

---
+++ ui.workspace/ui.base/src/demo/MyComponent.java
@@ -0,0 +1,23 @@
+package demo;
+
+import java.util.List;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import java.util.Arrays;
+import org.apache.felix.dm.annotation.api.Component;
+
+@Component(provides=Object.class)
+@Path("demo")
+public class MyComponent {
+
+	@GET
+    @Produces(MediaType.APPLICATION_JSON)
+   public List<String> list() {
+		
+		return Arrays.asList("test");
+	}
+	
+}
